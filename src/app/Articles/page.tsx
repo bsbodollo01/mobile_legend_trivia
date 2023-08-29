3de7f5cf89a95ad9dedf97d4374ca3e06371c3d2
Bryner Bodollo <bsbodollo01@gmail.com>
2023-08-29 14:57:31 +0800

    Refactor: re design article page

--- src/app/Articles/page.tsx
+++ src/app/Articles/page.tsx
@@ -3,10 +3,13 @@ import React from "react";
 const ArticlesView = () => {
     return (
         <div className="">
-            <p className="w-[40vw] text-4xl flex mx-auto text-center leading-relaxed py-[15vh]">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-            </p>
+            <div className=" py-[15vh]">
+                <p className="w-[40vw] text-4xl mx-auto text-center leading-relaxed">
+                    “Strength does not come from the flesh, but from an
+                    unyielding will.”
+                </p>
+                <h1 className="text-xl text-center pt-5">- Baxia</h1>
+            </div>
             <div className="flex flex-col gap-y-7">
                 <h1 className="text-4xl font-bold">Articles</h1>
                 <p className="w-[30vw] tracking-wider">
